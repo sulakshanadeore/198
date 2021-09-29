c17bc10959f7279f1d4f9cd6f8b5dded6bb02ac5
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-29 12:15:06 +0530

    Calling Class Library by referencing

--- Program.cs
+++ Program.cs
@@ -1,59 +1,19 @@
 ﻿using System;
-
-namespace ConsoleApp1
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MphasisMathsLibrary;
+namespace MyFirstconsolApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-
-            //object o;
-            //Console.WriteLine("Hello world");
-            int i,j,k=0;
-           // short j;
-            Console.WriteLine("Enter a  value for i");
-            //Convert string to integer
-            i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the value for j");
-            j = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter 1 to add \n2 to subtract \n3 multiply \n4 divide \n5 exit");
-            int choice = Convert.ToInt32(Console.ReadLine());
-                   
-
-            switch (choice)
-            {
-                case 1:
-                    k = i + j;
-                    break;
-                case 2:
-                    if(i!=0)
-                    k = i -j;
-                    break;
-                case 3:
-                    k = i * j;
-                    break;
-                case 4:
-                    if (j!= 0)
-                        k =i /j;
-                    break;
-                case 5:
-                    Environment.Exit(1);
-                    break;
-                
-            }
-                   
-            
-            Console.WriteLine("The answer =" + k);
-
-
-
-            //j = Convert.ToInt16(Console.ReadLine());
-
-            Console.ReadLine();
-
-
+            Operations.cube(2);
+            Operations.SquareRoot(144);
+            Operations.PowerOfNumber(2, 4);
+            Console.Read();
 
         }
     }
