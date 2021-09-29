a371bcc8b8090634d72bdecb55392944a60c612a
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-29 15:05:07 +0530

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MphasisMathsLibrary
+{
+    public class Car
+    {
+        //properties--color,model,name,price,fuelType,etc
+        //Behavior-- Drive,ApplyBrake,Reverse,standStill
+        //Events---Start,Stop,Accelerate,ChangeGear
+
+        //Variable is used to store data, and do operations on data
+        //Constant---- a value that won't change
+        string color;
+        string model;
+        string name;
+        string fuelType;
+        double price;
+        double average;
+
+        public Car()
+        {
+
+        }
+
+        //parameter and variable names are same--- this keyword is used for variable
+        public Car(string carname,string carcolor)
+        {
+            color = carcolor;
+            name = carname;
+        }
+
+        //Constructor chaining--- parameter names must match and sequence also shud match
+        public Car(string model, string carname,string carcolor):this(carname, carcolor)
+        {
+            this.model = model;
+            //Console.WriteLine(this.color);
+            //Console.WriteLine(this.name);
+            //Console.WriteLine(this.model);
+        }
+        public Car(string model, string carname, string carcolor,double price,string fuelType):this(model,carname,carcolor)
+        {
+            this.fuelType = fuelType;
+            this.price = price;
+            //Console.WriteLine(this.fuelType);
+            //Console.WriteLine(this.price);
+            //Console.WriteLine(this.average);
+        }
+
+
+        public Car(string model,double p)
+        {
+            this.model = model;
+            this.price = p;
+        }
+
+        public void ShowCarDetails()
+        {
+            Console.WriteLine("car Color =" + color);
+            Console.WriteLine("Model" + model);
+            Console.WriteLine(name);
+            Console.WriteLine(fuelType);
+            if (fuelType=="Petrol")
+            {
+                average = 20;
+            }
+            else
+            {
+                average = 19;
+            }
+            Console.WriteLine(average);
+            Console.WriteLine(name);
+            Console.WriteLine(price);
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MphasisMathsLibrary;
+namespace MyFirstconsolApp
+{
+    class Program
+    {
+        int s;
+
+        static void Main(string[] args)
+        {
+            //Car mycar = new Car();//Object creation
+
+            //Default constructor
+            //Its created by default when class is created
+
+
+            Car mycar = new Car("LXi", "Swift", "Red",500000,"Petrol");
+
+            mycar.ShowCarDetails();
+
+
+
+
+
+            //Console.WriteLine("Program Class = " + Operations.cnt);//1
+            ////Operations.cnt = Operations.cnt + 1;
+            ////Console.WriteLine(Operations.cnt);
+            ////Operations.cube(2);
+            //Operations.SquareRoot(144);
+            //Console.WriteLine("Program class=" + (Operations.cnt +1));
+            //Operations o = new Operations();
+            //o.ShowCounter();
+
+            //Program p = new Program();
+            //p.s
+            //Operations.PowerOfNumber(2, 4);
+            Console.Read();
+
+        }
+    }
+}
