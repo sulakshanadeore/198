7814e22a375c0631fd4e77ecf3dc754f81e4b312
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-29 17:14:26 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MphasisMathsLibrary
+{
+  public class Person
+    {
+
+        private int _personid;
+        //Accept value-- constructor
+        //    Print value--Method
+        //Smart method--- property-- read/write feature
+        public int PersonId
+        {
+            get { return _personid; }//return the value & printing it
+            set
+            {
+                _personid= value; 
+            if(_personid<0)
+                {
+
+                    Console.WriteLine("not a valid person id");
+                }
+            
+            }//assign the value to the variable to process 
+        
+        }
+
+        private string _personname;
+        public string PersonName
+        {
+            get { return _personname; }
+            set { _personname = value; }
+        
+        }
+
+
+        private DateTime _birthdate;
+        public DateTime BirthDate
+        {
+            //read/write properties
+            get { return _birthdate; }
+            set { _birthdate = value; }
+        }
+
+        private int _age;
+        public int Age
+        {
+            //Read only property
+            get {
+                _age = DateTime.Now.Year - _birthdate.Year;
+                return _age;
+            }
+        }
+
+        private string _password;
+        public string SetYourPassword
+        {
+            //Write password --variable _password, write only prop
+            set
+            {
+                if (value.Length > 0)
+                {
+                    Console.WriteLine("Password is assigned");
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Password length must be greater than zero");
+                }
+            }
+        }
+                
+                
+            
+
+            public void ShowPersonDetails()
+        {
+            Console.WriteLine(PersonId);
+            Console.WriteLine(PersonName);
+            Console.WriteLine(Age);
+            Console.WriteLine(BirthDate);
+            Console.WriteLine("Password is not shown for security reason");
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MphasisMathsLibrary;
+namespace MyFirstconsolApp
+{
+    class Program
+    {
+        int s;
+
+        static void Main(string[] args)
+        {
+
+
+            Person p = new Person();
+            Console.WriteLine("enter person id");
+            p.PersonId =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            
+            p.PersonName = Console.ReadLine();
+            Console.WriteLine("Enter your birth date");
+            p.BirthDate = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter your password");
+            p.SetYourPassword = Console.ReadLine();
+
+            Console.WriteLine("Print Details");
+            p.ShowPersonDetails();
+            //Console.WriteLine(p.PersonId);
+            //Console.WriteLine(p.PersonName);
+            //Console.WriteLine(p.BirthDate);
+            //Console.WriteLine(p.Age);
+            //Car mycar = new Car();//Object creation
+
+            //Default constructor
+            //Its created by default when class is created
+
+            //Console.WriteLine("Enter Manufacturing Date");
+            //DateTime dt = Convert.ToDateTime(Console.ReadLine());
+            //if (dt < DateTime.Now)
+            //{
+
+
+            //    Car mycar = new Car("LXi", "Swift", "Red", 500000, "Petrol", dt);
+            //    mycar.ShowCarDetails();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("date must be smaller than todays date");
+            //    Console.WriteLine("Todays date  = " + DateTime.Now);
+            //    Console.WriteLine("Month =" + DateTime.Now.Month);
+            //    Console.WriteLine("Day= " + DateTime.Now.Day);
+            //    Console.WriteLine("Date= " + DateTime.Now.Date );
+            //    Console.WriteLine("Day of week - " + DateTime.Now.DayOfWeek);
+            //    Console.WriteLine("Dya of Year =" + DateTime.Now.DayOfYear );
+            //    Console.WriteLine("Add 2 days to todays date = " + DateTime.Now.AddDays(2));
+            //}
+            
+
+
+
+
+
+            //Console.WriteLine("Program Class = " + Operations.cnt);//1
+            ////Operations.cnt = Operations.cnt + 1;
+            ////Console.WriteLine(Operations.cnt);
+            ////Operations.cube(2);
+            //Operations.SquareRoot(144);
+            //Console.WriteLine("Program class=" + (Operations.cnt +1));
+            //Operations o = new Operations();
+            //o.ShowCounter();
+
+            //Program p = new Program();
+            //p.s
+            //Operations.PowerOfNumber(2, 4);
+            Console.Read();
+
+        }
+    }
+}
