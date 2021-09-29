33d74999c693da1bac0f763e2179b33fce3cc88e
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-29 12:14:33 +0530

    This is to be written in Class library

---
+++ Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MphasisMathsLibrary
+{
+    public class Operations
+    {
+        public static void SquareRoot(double no)
+        {
+            double ans=Math.Sqrt(no);
+            Console.WriteLine(ans);
+        }
+        public static void cube(double no)
+        {
+            double ans = no * no * no;
+            Console.WriteLine(ans);
+        }
+        public static void PowerOfNumber(int n, int p)
+        {
+            double ans=Math.Pow(n, p);
+            Console.WriteLine(ans);
+        }
+
+    }
+}
