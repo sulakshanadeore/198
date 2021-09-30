ab0ef50d00311fd77c5bbb8215e2f2fe4db21d8e
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-30 12:43:52 +0530

    Add files via upload

--- ProductApp.cs
+++ ProductApp.cs
@@ -10,14 +10,22 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            //LaptopAccessories laptopAccessories = new LaptopAccessories("Latitude");
+            //LaptopAccessories laptop = new ElectronicProducts();
+          //  ElectronicProducts laptop = new LaptopAccessories();
+            //laptop-static
+           
+           
+
             Products p = new Products();
-            p.ProductID = 1;
-            p.ProductName = "Mouse";
-            p.ProductPrice = 700;
-            p.Qty = 11;
+            //p.ProductID = 1;
+            //p.ProductName = "Mouse";
+            //p.ProductPrice = 700;
+            //p.Qty = 11;
             
-            Console.WriteLine("Invoie Amt=" + p.InvAmt);
-            Console.WriteLine("Discount= " + p.Discount);
+
+            //Console.WriteLine("Invoie Amt=" + p.InvAmt);
+            //Console.WriteLine("Discount= " + p.Discount);
             Console.Read();
            
         }
