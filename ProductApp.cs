83d93fcc86817868156aa211e1d8b2996c5668e8
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-30 11:17:40 +0530

    WorkingWithProperties

---
+++ ProductApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApp2
+{
+    class ProductApp
+    {
+        static void Main(string[] args)
+        {
+            Products p = new Products();
+            p.ProductID = 1;
+            p.ProductName = "Mouse";
+            p.ProductPrice = 700;
+            p.Qty = 11;
+            
+            Console.WriteLine("Invoie Amt=" + p.InvAmt);
+            Console.WriteLine("Discount= " + p.Discount);
+            Console.Read();
+           
+        }
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Products
+    {
+        private int _prodid;
+
+        public int ProductID
+        {
+            get { return _prodid; }
+            set { _prodid = value; }
+        }
+
+        private string _prodname;
+
+        public string ProductName
+        {
+            get { return _prodname; }
+            set { _prodname = value; }
+        }
+
+        private float _price;
+
+        public float ProductPrice
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        private int _qty;
+
+        public int Qty
+        {
+            get { return _qty; }
+             set { _qty = value; }
+        }
+
+        private double _amt;
+
+        public double InvAmt
+        {
+            get {
+                CalculatePriceAfterDiscount(ProductPrice, Qty);
+                return _amt; }
+            private set { _amt = value; }
+        }
+
+        private double _dis;
+       
+
+        public double Discount
+        {
+            get { return _dis; }
+            private set { _dis = value; }
+        }
+
+
+        internal void CalculatePriceAfterDiscount(float price, int qty)
+        {
+
+            if (qty>10)
+            {
+                Discount= (price * qty) * .05 ;
+                InvAmt = (price * qty) - Discount;
+            }
+            else
+            {
+                InvAmt = (price * qty);
+            }
+        }
+
+
+
+
+
+    }
+}
