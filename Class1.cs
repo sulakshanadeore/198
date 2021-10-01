67884cd0981b00c0550056c9f888979bc84f1c11
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-01 16:29:05 +0530

    Generic- List Customer Demo

--- Class1.cs
+++ Class1.cs
@@ -6,83 +6,21 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    interface IVehDetails
+    public class Customer
     {
-        
-  int NoOfDoors { get; set; }
- string Type { get; set; }
-
-    }
-
-    public abstract class Vehicle:IVehDetails
-    {
-
-        public abstract string Vehno { get; set; }
-        //public double Average { get; set; }
-        public string Model { get; set; }
-        public string Name { get; set; }
-        public string RegNo { get; set; }
-        public abstract int NoOfDoors { get; set; }
-        public string Type { get; set; }
-
-        public double CalculateAverage(double distanceTravelled,double speed, int fuelConsumption)
+        public Customer()
         {
-            double avg=0;
-            avg=(distanceTravelled * speed) / fuelConsumption;
 
-            return avg;
-          //  Average = avg;
-        }
-        
-
-
-    }
-
-    public class Car : Vehicle
-    {
-        string _vehno;
-        public override string Vehno
-        {
-            get {
-
-                return _vehno;
-
-            }
-            set {
-                _vehno = value;
-                //Console.WriteLine("Enter State Name");
-                string stname = _vehno;
-                string s=stname.Substring(0, 2);
-                Random r = new Random(9);
-                string no=r.Next().ToString();
-                    _vehno = s + no.Substring(0, 4);
-
-            }
         }
 
-        int _doors=4;
-        public override int NoOfDoors
+        public Customer(int customerid,string customername,string customerCity)
         {
-            get { return _doors; }
-            set { _doors = value; }
+            this.Custid = customerid;
+            this.Custname = customername;
+            this.City = customerCity;
         }
-    }
-
-  public  class TwoWheeler : Vehicle
-    {
-        string _vehno;
-        public override string Vehno
-        {
-            get {
-                return _vehno;
-            }
-             set { _vehno = value;
-                Car car = new Car();
-                car.Vehno = _vehno;
-                
-                _vehno = car.Vehno + 1;
-            }
-        }
-        public override int NoOfDoors { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Custid { get; set; }
+        public string Custname { get; set; }
+        public string City { get; set; }
     }
 }
---
+++ GenericsDemo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace CollectionsDemo
+{
+    class GenericsDemo
+    {
+        static void Main(string[] args)
+        {
+            //Type casting overhead is saved
+            //List<int> li = new List<int>(); 
+            //li.Add(12);
+            //li.Add(243);
+            //foreach (var item in li)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            List<Customer> custlist = new List<Customer>();
+            Console.WriteLine("How many customer u need to add");
+            int count=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= count-1; i++)
+            {
+                Customer c = new Customer();
+                Console.WriteLine("Enter customer id");
+                c.Custid = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter customer name");
+                c.Custname = Console.ReadLine();
+                Console.WriteLine("Enter Customer city");
+                c.City = Console.ReadLine();
+                custlist.Add(c);
+            }
+            
+
+            
+            //custlist.Add(new Customer {Custid=2,Custname="RAJ",City="Pune" });
+            //custlist.Add(new Customer(3, "JAI", "DELHI"));
+
+
+            foreach (var item in custlist)
+            {
+                Console.WriteLine(item.Custid);
+                Console.WriteLine(item.Custname);
+                Console.WriteLine(item.City);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
