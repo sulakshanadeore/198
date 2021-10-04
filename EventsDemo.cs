9822c73f385d53f344e7e075bb6939817159d6a4
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-04 12:55:37 +0530

    Second version

--- EventsDemo.cs
+++ EventsDemo.cs
@@ -11,12 +11,24 @@ namespace EventsDelegatesDemo
         public static event StringWorking DoEvent;
         static void Main(string[] args)
         {
-         Maths m1 = new Maths();
-         StringWorking[] delarr1 = new StringWorking[3] { m1.ConcatStrings, m1.ReplaceStrings, m1.LengthOfStrings };
-             StringWorking finalDel1 = (StringWorking)MulticastDelegate.Combine(delarr1);
 
-            DoEvent += finalDel1;//subscribing to event
-            DoEvent("Hello", "Sita");//Invoke the event
+            Maths m1 = new Maths();
+            DoEvent += EventsDemo_DoEvent;
+            DoEvent("Hello", "Sita");//invoking event passing actual parameters
+
+            //De-couple---- remove from  each other (-=)
+            //Couple----add each other(+=)
+
+
+           //DoEvent  += new StringWorking(m1.ReplaceStrings);
+           // DoEvent("Hello", "Sita");
+            //int ans=DoEvent(10, 20);
+            //Console.WriteLine(ans);
+
+            //StringWorking[] delarr1 = new StringWorking[3] { m1.ConcatStrings, m1.ReplaceStrings, m1.LengthOfStrings };
+            //StringWorking finalDel1 = (StringWorking)MulticastDelegate.Combine(delarr1);
+            // DoEvent = finalDel1;//subscribing to event
+            //DoEvent("Hello", "Sita");//Invoke the event
             Console.Read();
 
 
@@ -24,6 +36,14 @@ namespace EventsDelegatesDemo
 
         }
 
-        
+        private static void EventsDemo_DoEvent(string s1, string s2)
+        {
+            //throw new NotImplementedException();
+            Maths m1 = new Maths();
+            StringWorking[] delarr1 = new StringWorking[3] { m1.ConcatStrings, m1.ReplaceStrings, m1.LengthOfStrings };
+            StringWorking finalDel1 = (StringWorking)MulticastDelegate.Combine(delarr1);
+            finalDel1(s1, s2);
+
+        }
     }
 }
