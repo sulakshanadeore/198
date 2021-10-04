1ad23a77a196eeb46e94e8037634a7ae474f9246
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-04 12:11:37 +0530

    Events calling Delegates

---
+++ EventsDemo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelegatesDemo;
+namespace EventsDelegatesDemo
+{
+    class EventsDemo
+    {
+        public static event StringWorking DoEvent;
+        static void Main(string[] args)
+        {
+         Maths m1 = new Maths();
+         StringWorking[] delarr1 = new StringWorking[3] { m1.ConcatStrings, m1.ReplaceStrings, m1.LengthOfStrings };
+             StringWorking finalDel1 = (StringWorking)MulticastDelegate.Combine(delarr1);
+
+            DoEvent += finalDel1;//subscribing to event
+            DoEvent("Hello", "Sita");//Invoke the event
+            Console.Read();
+
+
+            
+
+        }
+
+        
+    }
+}
