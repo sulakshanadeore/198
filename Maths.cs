8f756c064bb0907bcfc5af81903c52d53de3216c
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-04 10:47:50 +0530

    Delegates_UniCast_Multicast

---
+++ Maths.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    //declare the delegate
+    public delegate void StringWorking(string s1, string s2);
+    public delegate int AddDelegate(int x, int y);
+    public class Maths
+    {
+
+        public void ConcatStrings(string fs, string ss)
+        {
+           string newstr=string.Concat(fs, ss);
+            Console.WriteLine(newstr);
+        }
+
+
+        public void ReplaceStrings(string fs, string os)
+        {
+            string r1=fs.Replace('e', 'a');
+            string r2=os.Replace('i', 'e');
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+        }
+
+
+        public void LengthOfStrings(string fs, string s)
+        {
+            int l=fs.Length;
+            int l1 = s.Length;
+            int totalLength = l + l1;
+            Console.WriteLine(totalLength);
+
+        }
+        
+        public int Addition(int i, int j)
+        {
+            return i + j;
+        }
+
+        public int Subtraction(int i, int j)
+        {
+            int ans = 0;
+            if (i!=0)
+            {
+                ans= i - j;
+            }
+            return ans;
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelegatesDemo;
+namespace EventsDelegatesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Maths m = new Maths();
+            //pass address of method to delegate
+            //If a  delegate calls single method---- Unicast delegate
+            AddDelegate del = new AddDelegate(m.Subtraction);
+
+            //invoke the delegate 
+            //int delans=del(20, 2);
+            int delans=del.Invoke(20, 2);
+
+            Console.WriteLine(delans);
+
+
+            //Multi-cast---- call multiple methods by using single delegate
+            //Multi-cast delegate return type must always be void.
+            //Chain of  multiple method address-----so that they get called one AFTER the other--- synchronous invocation
+            Console.WriteLine("--------------------------");
+            StringWorking sw1 = new StringWorking(m.ConcatStrings);
+            StringWorking sw2 = new StringWorking(m.ReplaceStrings);
+          StringWorking sw3=(StringWorking) MulticastDelegate.Combine(sw1, sw2);
+
+
+            //sw3("Hello", "Sita");
+            sw3.Invoke("Hello", "Sita");
+
+            Console.WriteLine("---------------Array of delegates--------");
+
+            StringWorking[] delarr = new StringWorking[3] {m.ConcatStrings,m.ReplaceStrings,m.LengthOfStrings };
+            StringWorking finalDel=(StringWorking)MulticastDelegate.Combine(delarr);
+            //finalDel("Hello", "Babita");
+            finalDel.Invoke("Hello", "Babita");
+
+
+            //asynchronous invocation------ one method get called that is taking a long time to execute and so UNTIL it completes we call the next method in the sequence and
+            //then check whether first method has completed and as per the status continue accordingly.
+
+            //A_Method(){
+
+            //}
+            //B_Method(){    }
+
+
+
+            Console.Read();
+
+        }
+    }
+}
