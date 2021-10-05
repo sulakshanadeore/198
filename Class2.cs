4847d6d688bcdc6c0738e9ec1a92b05c036d7340
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-05 17:59:32 +0530

    Partial Demo

---
+++ Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    public partial class student
+    {
+        public int rollno { get; set; }
+        private int _v1;
+
+        public void M1()
+        {
+            P1();
+
+        }
+
+
+        partial void P1()
+        {
+            _v1 = 10;
+            Console.WriteLine("Hello");
+            Console.WriteLine(_v1);
+
+        }
+
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDemo
+{
+    public partial class student
+    {
+        public int marks { get; set; }
+        public string name { get; set; }
+
+        partial void P1();
+    }
+}
