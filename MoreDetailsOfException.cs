80a6dd9b0482d2bdb3ca7f5956a3d3613a988ee1
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-06 10:10:03 +0530

    Add files via upload

---
+++ MoreDetailsOfException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExceptionHandlingDemo;
+namespace ConsoleApp1
+{
+    class MoreDetailsOfException
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter no1");
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter no2");
+                int j = Convert.ToInt32(Console.ReadLine());
+
+
+                Demo d = new Demo();
+                d.divide(i, j);
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                Console.WriteLine("Message = " + ex.Message);
+                Console.WriteLine("Inner Exception =" + ex.InnerException);
+                Console.WriteLine("Source= " + ex.Source);
+                Console.WriteLine("Stack Trace= " + ex.StackTrace);
+                Console.WriteLine("Target Site= " + ex.TargetSite);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Message = " + ex.Message);
+                Console.WriteLine("Inner Exception =" + ex.InnerException);
+                Console.WriteLine("Source= " + ex.Source);
+                Console.WriteLine("Stack Trace= " + ex.StackTrace);
+                Console.WriteLine("Target Site= " + ex.TargetSite);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
