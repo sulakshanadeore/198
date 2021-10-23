70ab055b5983776c2575588c34465e8e1ec46210
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-23 16:33:24 +0530

    Add files via upload

---
+++ Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StateMgtDemo
+{
+    public class Products
+    {
+        public int Prodid { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
