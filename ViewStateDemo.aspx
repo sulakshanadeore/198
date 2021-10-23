82c161913d8a052c32ac0bfb686cf563582d6e9c
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-23 12:55:54 +0530

    View state demo

---
+++ ViewStateDemo.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewStateDemo.aspx.cs" Inherits="StateMgtDemo.ViewStateDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Enter Name:"></asp:Label>
+&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Number of hits" />
+&nbsp;
+        <asp:Label ID="lblhits" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ViewStateDemo.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateMgtDemo
+{
+    public partial class ViewStateDemo : System.Web.UI.Page
+    {
+        int cnt = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+                lblhits.Text = "Not hits yet";
+            }
+            else
+            {
+                //ViewState --- statebag that can hold values in key value pairs, it uses dictionary.
+                //ViewState can store object type of data
+                ViewState["hitscount"] = Convert.ToInt32(ViewState["hitscount"]) + 1;
+                cnt = Convert.ToInt32(ViewState["hitscount"]);
+                lblhits.Text = ViewState["hitscount"].ToString();
+                if (Convert.ToInt32(ViewState["hitscount"]) >= 3)
+                {
+                    Button1.Enabled = false;
+                    Response.Write("U have exceeded the maximum limit of 3");
+                }
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+           
+
+        }
+    }
+}
