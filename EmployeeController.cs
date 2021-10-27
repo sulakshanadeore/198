527b3ad5b38f4fd59b7297c68c6cc367a7022af6
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-27 17:10:37 +0530

    ViewBag,ViewData,TempData-StateMgt

---
+++ EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            ViewBag.mydata = "got data from controller";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(int id)
+        {
+            TempData["v1"] = "Hello from Index";
+            return RedirectToAction("Create");
+           }
+        public ActionResult Create()
+        {
+            TempData.Keep();
+            string s=TempData["v1"].ToString();
+            return Content(s);
+        }
+        public ActionResult Greet()
+        {
+                               //pickup
+            string s1=TempData.Peek("v1").ToString();
+            return Content(s1);
+
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(int i,int j)
+        {
+            int c = i + j;
+            ViewBag.answer = c;
+            return View();
+        }
+
+        public ActionResult subtract()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult subtract(int i,int j)
+        {
+            int c = i - j;
+            ViewData["answer"] = c;
+            return View();
+        }
+
+
+
+
+    }
+}
