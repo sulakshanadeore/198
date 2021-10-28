37593e3e5e2a34bac847fb5406683546651dd810
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-10-28 13:15:16 +0530

    Caching with Profiles

---
+++ Emp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CachingDemo.Models
+{
+    public class Emp
+    {
+
+        public int empid { get; set; }
+        public string empName { get; set; }
+    }
+}
--- EmployeeController.cs
+++ EmployeeController.cs
@@ -3,63 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
-namespace FirstMVCApp.Controllers
+using CachingDemo.Models;
+namespace CachingDemo.Controllers
 {
     public class EmployeeController : Controller
     {
+        public static List<Emp> emps = new List<Emp>();
+        static EmployeeController()
+        {
+            emps.Add(new Emp {empid=1,empName="A" });
+            emps.Add(new Emp { empid = 2, empName = "B" });
+            emps.Add(new Emp { empid = 3, empName = "C" });
+            emps.Add(new Emp { empid = 4, empName = "D" });
+
+
+        }
         // GET: Employee
-        public ActionResult Index()
-        {
-            ViewBag.mydata = "got data from controller";
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Index(int id)
-        {
-            TempData["v1"] = "Hello from Index";
-            return RedirectToAction("Create");
-           }
-        public ActionResult Create()
-        {
-            TempData.Keep();
-            string s=TempData["v1"].ToString();
-            return Content(s);
-        }
-        public ActionResult Greet()
-        {
-                               //pickup
-            string s1=TempData.Peek("v1").ToString();
-            return Content(s1);
-
-        }
-
-        public ActionResult Add()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Add(int i,int j)
-        {
-            int c = i + j;
-            ViewBag.answer = c;
-            return View();
-        }
-
-        public ActionResult subtract()
+        public ActionResult Search()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult subtract(int i,int j)
+        //[OutputCache(Duration = 10, VaryByParam = "id")]
+        [OutputCache(CacheProfile ="c2")]
+        public ActionResult Search(int id)
         {
-            int c = i - j;
-            ViewData["answer"] = c;
+            var m = (from a in emps
+                    where a.empid == id
+                    select a).FirstOrDefault();
+            ViewBag.id = m.empid;
+            ViewBag.name = m.empName;
             return View();
         }
-
-
 
 
     }
