352cb4318213244ee22a999d9d4ac40e74b7ae33
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-11-01 10:50:33 +0530

    Consume web service

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsumeWebServiceDemo.ServiceReference1;
+namespace ConsumeWebServiceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient proxy = new WebService1SoapClient();
+            Employee[] employees=proxy.ShowRecords("SPD","northwind","employees","", "");
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item.Empid);
+                Console.WriteLine(item.Empname);
+            }
+            Console.Read();
+        }
+    }
+}
