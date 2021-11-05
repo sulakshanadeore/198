66fbf19f51ed8033c751c6a6ab1453a800855116
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-11-05 10:10:33 +0530

    Validations using Attributes

---
+++ CompanyDetailsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VallidationsDemo.Models
+{
+    public class CompanyDetailsModel
+    {
+        [Required]
+        [Display(Name ="Enter Company ID:",Order =0)]
+        public int CompanyID { get; set; }
+
+        
+        [Display(Order = 4,Name = "Enter No of Employees:")]
+        [Required]
+        [Range(2, 2000)]
+        public int NoOfEmployees { get; set; }
+
+
+        [Display(Name = "Enter Company Name:",Order =1)]
+        [MinLength(2,ErrorMessage ="Name must be min 2 chars long")]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "Enter City:",Order =2)]
+        public string City { get; set; }
+
+        
+     
+
+    }
+}
