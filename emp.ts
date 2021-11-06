ea1661f73d7c6a9ae80adcd8ba7ccbe02f9f1a20
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-11-06 16:59:34 +0530

    static demo

--- emp.ts
+++ emp.ts
@@ -1,8 +1,18 @@
 class employee {
+    private static empcount:number=0;
 private    _empid:number;
 private    _empname:string;
+public getEmpCount():number
+{
+return employee.empcount;
 
-      
+}
+     
+constructor()
+{
+employee.empcount=employee.empcount+1;
+
+}
     // constructor(empno:number,ename:string) {
     //     this._empid=empno;
     //     this._empname=ename;
@@ -34,8 +44,22 @@ e1.employeeid=1;
 e1.employeename="Sumit";
 console.log("U entered employee id= "  +e1.employeeid);
 console.log("U entered employee name= "  +e1.employeename);
-
 e1.display();
+var c:number=e1.getEmpCount();
+console.log("Employee Count=" + c); 
+
+var e2=new employee();
+e2.employeeid=2;
+e2.employeename="Kabir";
+console.log("U entered employee id= "  +e2.employeeid);
+console.log("U entered employee name= "  +e2.employeename);
+e2.display();
+var c:number=e2.getEmpCount();
+console.log("Employee count= " + c);
+
+var e3=new employee();
+var c:number=e3.getEmpCount();
+console.log("Employee count= " + c);
 // var e1=new employee(1,"Poonam");
 // var e2=new employee(2,"Sumit");
 // e1.display();
